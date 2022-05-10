4cb1afcd0c5698140f0cb38e722477a6cba26bb5
Luciano Riquelme <lucianoriquelme519@gmail.com>
2022-05-10 07:47:28 -0300

    Reactualizacion de las clases Arreglo y PruebaArreglo

--- src/riquelme/tp5/Arreglo.java
+++ src/riquelme/tp5/Arreglo.java
@@ -16,53 +16,89 @@ public class Arreglo {
     }
     
     public static int buscarMayor(int matriz[][]) {
-        int max[][];
-        /*
-        for(int  f=0 ;f<matriz[f].length;f++)
-            for(int c=0;c<matriz[f].length;c++)
-                if(matriz[f][c]>max)
-                    max=matriz[f][c];
+        int max=matriz[0][0];
         
-        System.out.print("El numero mas grande: "+max[][]);
-        */        
-        return 0;
-    }
-    
-    public static int cuentaVocales(String cadena[]) {
-        int vocales=0;
-        for(int i=0;i<cadena.length;i++) {
-            switch(cadena[i]){
-                case "a":
-                case "A":
-                    vocales++;
-                    break;
-                case "e":
-                case "E":
-                    vocales++;
-                    break;
-                case "i":
-                case "I":
-                    vocales++;
-                    break;
-                case "o":
-                case "O":
-                    vocales++;
-                    break;
-                case "u":
-                case "U":
-                    vocales++;
-                    break;
-                default:
-                    if(vocales==0)
-                        System.out.println("No se encontraron vocales.");
-                    break;
+        for (int i = 0; i< matriz.length; i++) 
+            for (int j = 0; j <matriz[i].length; j++) {
+                if (matriz[i][j]>max) {
+                    max=matriz[i][j];
+                }
             }
-        } 
-        System.out.println(vocales);
+        
+        return max;
+    }
+    
+    public static int cuentaVocales(String palabra) {
+        int vocales=0;
+        
+        for (int i = 0; i < palabra.length(); i++) {
+            if(palabra.charAt(i)=='A' || palabra.charAt(i)=='a')
+                vocales++;
+            if(palabra.charAt(i)=='E'|| palabra.charAt(i)=='e')
+                vocales++;
+            if(palabra.charAt(i)=='I'|| palabra.charAt(i)=='i')
+                vocales++;
+            if(palabra.charAt(i)=='O' || palabra.charAt(i)=='o')
+                vocales++;
+            if(palabra.charAt(i)=='U' || palabra.charAt(i)=='u')
+                vocales++;
+        }
+        System.out.println("Se encontraron "+vocales+" vocales en la palabra '"+palabra+"'");
+        
         return 0;
     }
     
-    public static String cuentaCaracter(String cadena,char caracter){
-        return "";
+    public static int cuentaCaracter(String cadena,char caracter){
+        int counter=0;
+        
+        for (int i = 0; i < cadena.length(); i++) {
+            if(cadena.charAt(i)==caracter)
+                counter++;
+        }
+        System.out.println("Cantidad de veces encontrado el caracter '"+ caracter+"' en la cadena:"+counter);
+        return 0;
     }
 }
+
+/*
+ Scanner filas= new Scanner(System.in);
+        Scanner columnas= new Scanner(System.in);
+        Scanner numbers= new Scanner(System.in);
+        
+        int f;
+        int c;
+        int max;
+        
+        System.out.println("Ingrese numero de filas: ");
+        f=filas.nextInt();
+        
+        System.out.println("Ingrese numero de columnas: ");
+        c=columnas.nextInt();
+        
+        matriz=new int[f][c];
+        
+        
+        System.out.println("Llene la matriz con numeros enteros:");
+        for (int i = 0; i < f; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.println("["+i+"]["+j+"]");
+                 matriz[i][j]=numbers.nextInt();
+            }
+        }
+        
+        max=matriz[0][0];
+        
+        System.out.println("Matriz Ingresada:");
+        for(int x=0;x<f;x++) {
+            for(int y=0;y<c;y++) {
+                if(matriz[x][y]>max)
+                    max=matriz[x][y];
+                if(matriz[x][y]<10)
+                    System.out.print("0");
+                System.out.print(matriz[x][y]+"  ");
+            }
+            System.out.println("");
+        }
+        System.out.println("el numero mas grande de la matriz es: "+max);    
+         
+        return 0;*/
--- src/riquelme/tp5/PruebaArreglo.java
+++ src/riquelme/tp5/PruebaArreglo.java
@@ -1,12 +1,39 @@
 package riquelme.tp5;
 
+
+
 public class PruebaArreglo {
     
-    public static void main(String[] args) {
-        int lista[]={10,2,78,56,34};
-        String vocales[]={"a","e","i","o","u"};
+    public static void main(String[] args) {  
+        //variables a usar en los metodos
+        int lista[]={10,2,78,56,34}; //sumarLista()
+        
+       // int matriz[][];
+        //matriz=new int[100][100];
+        
+        
+        //Metodos estaticos de la clase Arreglo
+        System.out.println("Sumar Lista:");
         Arreglo.sumarLista(lista);
-        Arreglo.cuentaVocales(vocales);
+        
+        int[][] matriz2={{10,0,33,4},{566,66,7,8}};
+        
+        System.out.println("\nBuscando numero mas grande en una matriz:");
+        System.out.println("El numero mas grande encontrado:"+Arreglo.buscarMayor(matriz2));
+        
+        System.out.println("\nContar vocales de una palabra:");
+        String palabra="Hello";
+        Arreglo.cuentaVocales(palabra);
+        
+        String cadena="Bienvenidos a laboratorio de programacion 1";
+        
+        System.out.println("\nContar Caracteres encontrados en una cadena:");
+        System.out.println("Cadena ingresada:"+cadena);
+        Arreglo.cuentaCaracter(cadena, 'o');
+        Arreglo.cuentaCaracter(cadena, 'a');
+        Arreglo.cuentaCaracter(cadena, 'e');
+        Arreglo.cuentaCaracter(cadena, 'n');
+        System.out.println("\n\n\n ");
         
     }
 }
